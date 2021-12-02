66e27254cbad74e8e2dd78c137daf47144dfba92
RooRsmoka <chri.disa96@gmail.com>
2021-12-02 18:05:38 +0100

    creazione file e struttura esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* chiedo all'utente delle informazioni che salverò dentro delle costanti*/
+/* Chiedo all'utente il suo nome */
+/* Chiedo all'utente il suo cognome */
+/* Chiedo all'utente il suo colore preferito */
+/* prendo l'elemento #pwdgen all'interno del document e lo salvo in una variabile */
+/* imposto all'attributo innerHTML della variabile la concatenazione delle costanti inserite dall'utente e le ultime due cifre dell'anno corrente */
+
+
+
