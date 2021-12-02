6334485ce3ba57b0eb45ca2bf98044567c08d887
RooRsmoka <chri.disa96@gmail.com>
2021-12-02 18:11:17 +0100

    esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 /* chiedo all'utente delle informazioni che salverò dentro delle costanti*/
-/* Chiedo all'utente il suo nome */
-/* Chiedo all'utente il suo cognome */
-/* Chiedo all'utente il suo colore preferito */
-/* prendo l'elemento #pwdgen all'interno del document e lo salvo in una variabile */
-/* imposto all'attributo innerHTML della variabile la concatenazione delle costanti inserite dall'utente e le ultime due cifre dell'anno corrente */
 
+/* Chiedo all'utente il suo nome. */
+const userName = prompt('Inserisci il tuo nome.');
 
+/* Chiedo all'utente il suo cognome. */
+const userLastname = prompt('Inserisci il tuo cognome.');
 
+/* Chiedo all'utente il suo colore preferito. */
+const userFavoritecolor = prompt('inserisci il tuo colore preferito.');
+
+/* prendo l'elemento #pwdgen all'interno del document e lo salvo in una variabile. */
+let htmlElement = document.getElementById('pwdgen');
+
+/* imposto all'attributo innerHTML della variabile la concatenazione delle costanti
+   inserite dall'utente e le ultime due cifre dell'anno corrente tramite template literal.
+*/
+htmlElement.innerHTML = `${userName}${userLastname}${userFavoritecolor}21`;
